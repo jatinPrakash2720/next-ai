5f2756e1c3cceee440547584c87d40b9715456d6
jatinPrakash2720 <jatinprakashjdboss@gmail.com>
2025-10-22 00:53:07 +0530

    feat: Add username uniqueness check and verification code handling
    - Implemented a new API endpoint to check for unique usernames during registration.
    - Added validation for username input using Zod.
    - Created a verification code endpoint to validate user accounts.
    - Enhanced middleware to redirect authenticated users away from sign-in and sign-up pages to the dashboard.

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,51 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, code } = await request.json();
+    //decodeURIComponent(username) use this, if you are using query from search url, as it add %20
+    const decodedUsername = decodeURIComponent(username);
+    const user = await UserModel.findOne({ username: decodedUsername });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 403 }
+      );
+    }
+    const isCodeValid = user.verifyCode === code
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+
+    if(isCodeNotExpired && isCodeValid){
+        user.isVerified = true
+        await user.save()
+
+        return Response.json({
+            success:true,
+            message:"Account Verified successfully"
+        },{status:200})
+    }else if(!isCodeNotExpired){
+        return Response.json({
+            success:false,
+            message:"Verification Code is Expired, please sign-up again to verify again"
+        },{status:400})
+    }else {
+        return Response.json({
+            success:false,
+            message:"Verification Code that you have sent is incorrect"
+        },{status:401})
+    }
+  } catch (error) {
+    console.error("Error while verifing code :", error);
+    return Response.json(
+      { success: false, message: "Error while verifing code" },
+      { status: 500 }
+    );
+  }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,33 @@
-import { NextResponse, NextRequest } from 'next/server'
-import { getToken } from "next-auth/jwt"
+import { NextResponse, NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({req: request})
-    const url = request.nextUrl
-    
-    // If user is not authenticated and trying to access protected routes, redirect to sign-in
-    if (!token && (url.pathname.startsWith('/dashboard') || url.pathname.startsWith('/verify'))) {
-        return NextResponse.redirect(new URL('/sign-in', request.url))
-    }
-    
-    return NextResponse.next()
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  // If user is not authenticated and trying to access protected routes, redirect to sign-in
+  if (
+    !token &&
+    (url.pathname.startsWith("/dashboard") ||
+      url.pathname.startsWith("/verify"))
+  ) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("sign-up") ||
+      url.pathname.startsWith("verify") ||
+      url.pathname.startsWith("/"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url))
+  }
+
+
+  return NextResponse.next();
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/dashboard/:path*', '/verify/:path*'],
-}
+  matcher: ["/dashboard/:path*", "/verify/:path*", "/sign-in","/sign-up"],
+};
