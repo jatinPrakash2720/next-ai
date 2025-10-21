5615a064d0a04d8b8bb84cdf335cae21d9279184
jatinPrakash2720 <jatinprakashjdboss@gmail.com>
2025-10-21 17:02:09 +0530

    feat: Enhance user registration process with email verification
    - Updated sign-up route to handle existing users and verification codes.
    - Implemented logic to check for existing verified and unverified users.
    - Added email sending functionality for verification after user registration.
    - Refactored user creation and password hashing logic for better clarity.

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,25 +1,102 @@
-import  dbConnect  from "@/lib/dbConnect";
-import { User } from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
-
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 
-export async function POST(request:Request){
-    await dbConnect()
+export async function POST(request: Request) {
+  await dbConnect();
 
-    try {
-        const {username, email, password}=await request.json()
-        
-    } catch (error) {
-        console.error('Error registering user', error)
-        return Response.json(
-            {
-                success:false,
-                message:'Error while registering User'
-            },
-            {
-                status:500
-            }
-        )
+  try {
+    const { username, email, password } = await request.json();
+
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        { success: false, message: "Username is already taken" },
+        { status: 400 }
+      );
     }
+
+    const existingUnverifiedUserByEmail = await UserModel.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUnverifiedUserByEmail) {
+      if (existingUnverifiedUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: "User already exist with this email",
+          },
+          { status: 400 }
+        );
+    }else{
+        const hashedPassword = await bcrypt.hash(password,10)
+        existingUnverifiedUserByEmail.password = hashedPassword
+        existingUnverifiedUserByEmail.verifyCode = verifyCode
+        existingUnverifiedUserByEmail.verifyCodeExpiry = new Date(Date.now()+3600000)
+
+        await existingUnverifiedUserByEmail.save()
+        
+    }
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModel({
+        username: username,
+        email: email,
+        password: hashedPassword,
+        verifyCode: verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+
+      await newUser.save();
+    }
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User registered successfully. Please verify your email",
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.error("Error registering user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error while registering User",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
